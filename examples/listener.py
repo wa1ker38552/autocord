edee9e48ce7530d7ffc7dc1efc396264a024fe71
walker <100868154+wa1ker38552@users.noreply.github.com>
2023-03-01 19:00:18 -0800

    Create listener.py

---
+++ examples/listener.py
@@ -0,0 +1,13 @@
+import autocord
+
+client = autocord.Client('TOKEN')
+
+@client.event
+async def on_ready():
+  print(f"Logged in as: {client.user}")
+  
+@client.event
+async def on_message():
+  print(f"{message.author}: {message.content}")
+  
+client.run()
