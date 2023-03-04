e8a3aa7642f0097d82dd92bf7f83b53a0d0e7b83
walker <100868154+wa1ker38552@users.noreply.github.com>
2023-03-04 12:39:50 -0800

    Update deleted.py

--- examples/deleted.py
+++ examples/deleted.py
@@ -4,14 +4,14 @@ import autocord
 client = autocord.Client('TOKEN')
 data = {}
 
-@client.event()
+@client.event
 async def on_message_delete(message):
   if message.id in data:
     await client.send(message.channel.id, f"{data[message.id]['author']} deleted a message: {data[message.id]['content']}")
   else:
     await client.send(message.channel.id, "A message was deleted but it couldn't be located!")
     
-@client.event()
+@client.event
 async def on_message(message):
   data[message.id] = {
     "author": str(message.author),
